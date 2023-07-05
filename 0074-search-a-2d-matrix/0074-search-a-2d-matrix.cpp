84382fb8c4dbc8e597c9129b52aa05c7eaf48464
Himanshu2126 <93526524+Himanshu2126@users.noreply.github.com>
2023-07-05 12:30:50 +0530

    Time: 5 ms (64.29%), Space: 9.5 MB (21.96%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,62 +1,34 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target)
-     {
-
-         int n=matrix.size();
-         int m=matrix[0].size();
-         int l=0;
-         int h=m*n-1;
-
-
-      
-             if(!matrix.size()) return false;
-
-         while(l<=h)
-         {
-                int mid= l+(h-l)/2;
-
-                int r=mid/m;
-                int c=mid%m;
-             if(matrix[r][c]==target)
-             return true;
-
-             else if(matrix[r][c]<target)
-             {
-                 l=mid+1;
-             }
-             else
-             h=mid-1;
-         }
-
-
-         return false;
-
-        //   int i=0;
-        //   int j=m-1;
-        //  while(i<n && j>=0)
-
-
-
-        //   {   
-        //       if(target==matrix[i][j])
-        //       {
-        //           return true;
-
-        //       }
-        //       else if(target>matrix[i][j])
-        //       {
-        //         i++;
-
-        //       }
-        //       else 
-        //       j--;
-             
-
-        //    }
-
-        //    return false;
-
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
         
+
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int  rowindex=0;
+        
+
+        // loop for finding target row index;
+        for(int i=0;i<row;i++)
+        {
+            if(matrix[i][0]<=target && target<=matrix[i][col-1])
+            {
+                rowindex=i;
+            }
+
+        }
+
+
+
+         for(int j=0;j<col;j++)
+        {
+            if(matrix[rowindex][j]==target)
+            {
+                return true;
+            }
+
+        }
+        return false;
     }
 };

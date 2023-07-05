6a30c1baaeced96f7bd8b538894f6af2d8cf9ed7
Himanshu2126 <93526524+Himanshu2126@users.noreply.github.com>
2023-07-05 12:13:47 +0530

    Time: 10 ms (18.8%), Space: 9.4 MB (60.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,28 +5,57 @@ public:
 
          int n=matrix.size();
          int m=matrix[0].size();
+         int l=0;
+         int h=m*n-1;
 
-          int i=0;
-          int j=m-1;
-         while(i<n && j>=0)
-          {   
-              if(target==matrix[i][j])
-              {
-                  return true;
 
-              }
-              else if(target>matrix[i][j])
-              {
-                i++;
+      
+             if(!matrix.size()) return false;
 
-              }
-              else 
-              j--;
+         while(l<=h)
+         {
+                int mid= l+(h-l)/2;
+
+                int r=mid/m;
+                int c=mid%m;
+             if(matrix[r][c]==target)
+             return true;
+
+             else if(matrix[r][c]<target)
+             {
+                 l=mid+1;
+             }
+             else
+             h=mid-1;
+         }
+
+
+         return false;
+
+        //   int i=0;
+        //   int j=m-1;
+        //  while(i<n && j>=0)
+
+
+
+        //   {   
+        //       if(target==matrix[i][j])
+        //       {
+        //           return true;
+
+        //       }
+        //       else if(target>matrix[i][j])
+        //       {
+        //         i++;
+
+        //       }
+        //       else 
+        //       j--;
              
 
-           }
+        //    }
 
-           return false;
+        //    return false;
 
         
     }

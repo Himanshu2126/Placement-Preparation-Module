37b016eb8e481c56c27db7fa1bf2326bba07de80
Himanshu2126 <93526524+Himanshu2126@users.noreply.github.com>
2023-07-09 00:33:55 +0530

    Time: 9 ms (52.79%), Space: 7.6 MB (81.91%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -12,23 +12,53 @@ public:
 
 
    
-    map<ListNode *,bool>mp;
+    // map<ListNode *,bool>mp;
 
 
-     while(head)
-     {
-         if(mp.find(head)!=mp.end())
-         {
-             return head;
+    //  while(head)
+    //  {
+    //      if(mp.find(head)!=mp.end())
+    //      {
+    //          return head;
 
-         }
-         else
-         {
-            mp[head]=true;
-            head=head->next;
-         }
-     }
+    //      }
+    //      else
+    //      {
+    //         mp[head]=true;
+    //         head=head->next;
+    //      }
+    //  }
 
-        return NULL;
+    //     return NULL;
+    // }
+
+
+
+
+if(head == NULL||head->next == NULL) return NULL;
+        
+    ListNode* fast = head;
+   ListNode* slow = head;
+    ListNode* entry = head;
+        
+    while(fast->next != NULL&&fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+            
+        if(slow == fast) {
+            while(slow != entry) {
+                slow = slow->next;
+                entry = entry->next;
+            }
+            return slow;
+        }
     }
+    return NULL;
+
+    }
+
+
+
+
+
 };
